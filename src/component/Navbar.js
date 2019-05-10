a36f101eb512df7001f9c220f9f9144264a36124
Beau Hicken <Turbodiesel2500dee@hotmail.com>
2019-05-10 16:14:12 -0600

    made components

---
+++ src/component/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Navbar extends React.Component {
+    render() {
+        return (
+            <div>
+                Navbar page
+            </div>
+        )
+        
+    }
+}
+
+export default Navbar
---
+++ src/component/Services.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Services extends React.Component {
+    render() {
+        return (
+            <div>
+                Services page
+            </div>
+        )
+    }
+}
+
+export default Services
